3ed4567eaa8afd86388afe637848c78cd1a807ba
Mahisami <88837372+Mahisami@users.noreply.github.com>
2023-08-23 14:28:38 +0300

    Update Nav.js

--- SeblAi/frontend/src/components/Nav.js
+++ SeblAi/frontend/src/components/Nav.js
@@ -10,27 +10,14 @@ const NavigationBar = () => {
   };
 
   return (
-    <nav className="border-b border-b-[#e6ebf4] p-5 nav">
+    <nav className="border-b border-b-[#e6ebf4] p-4 lg:p-5 nav">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <img src={logo} alt="Logo" className="w-20 h-15 mr-4" />
         </div>
         <div className="flex items-center space-x-4">
-          <div className="hidden lg:flex space-x-4">
-<<<<<<< HEAD
-            <Link
-              to="/"
-              className="relative w-fit text-[#396E8D] block font-semibold after:block after:content-[''] after:absolute after:h-[3px] after:bg-slate-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
-              activeClassName="text-blue-500" 
-=======
-            <a href="#" className="relative w-fit text-[#396E8D] block font-semibold after:block after:content-[''] after:absolute after:h-[3px] after:bg-slate-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Home</a>
-            <a href="#" className="relative w-fit text-[#396E8D] block font-semibold after:block after:content-[''] after:absolute after:h-[3px] after:bg-slate-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">About</a>
-            <a href="#" className="relative w-fit text-[#396E8D] block font-semibold after:block after:content-[''] after:absolute after:h-[3px] after:bg-slate-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Contact</a>
-
-           
-          </div>
           <button
-            className="text-[#396E8D] lg:hidden"
+            className="lg:hidden text-[#396E8D] p-2"
             onClick={toggleMenu}
           >
             <svg
@@ -39,7 +26,20 @@ const NavigationBar = () => {
               stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
->>>>>>> d7235ab4223f3e1a8b5ab045692739559adbdad2
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+          <div className={`lg:flex space-x-4 ${menuOpen ? 'block' : 'hidden'}`}>
+            <Link
+              to="/"
+              className="relative w-fit text-[#396E8D] block font-semibold after:block after:content-[''] after:absolute after:h-[3px] after:bg-slate-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
+              activeClassName="text-blue-500" 
             >
               Home
             </Link>
@@ -58,20 +58,8 @@ const NavigationBar = () => {
               Contact
             </Link>
           </div>
-         
         </div>
       </div>
-<<<<<<< HEAD
-      
-=======
-      {menuOpen && (
-        <div className="lg:hidden mt-4">
-          <a href="#" className="block text-[#396E8D] font-semibold mb-2">Home</a>
-          <a href="#" className="block text-[#396E8D] font-semibold mb-2">About</a>
-          <a href="#" className="block text-[#396E8D] font-semibold mb-2">Contact</a>
-        </div>
-      )}
->>>>>>> d7235ab4223f3e1a8b5ab045692739559adbdad2
     </nav>
   );
 };
